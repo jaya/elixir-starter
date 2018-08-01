138922d28e7bdd11a91681c426e405c08c53d74a
Raul Pereira <raul.pereira@gmail.com>
2018-08-01 12:03:56 -0300

    new directory for raulpe7eira coaching

---
+++ apps/raulpe7eira/lib/raulpe7eira.ex
@@ -0,0 +1,18 @@
+defmodule Raulpe7eira do
+  @moduledoc """
+  Documentation for Raulpe7eira.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Raulpe7eira.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/raulpe7eira/test/raulpe7eira_test.exs
@@ -0,0 +1,8 @@
+defmodule Raulpe7eiraTest do
+  use ExUnit.Case
+  doctest Raulpe7eira
+
+  test "greets the world" do
+    assert Raulpe7eira.hello() == :world
+  end
+end
