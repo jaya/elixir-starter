c2cabe95f9b318b8a2cb890c5839bef49d2b048a
Raul Pereira <raul.pereira@gmail.com>
2018-08-02 15:42:24 -0300

    Step 2: samples of modules and functions - updated

--- apps/raulpe7eira/getting_started/concat.exs
+++ apps/raulpe7eira/getting_started/concat.exs
@@ -1,15 +1,11 @@
 defmodule Concat do
-  def join(a, b \\ nil, sep \\ " ")
-
-  def join(a, b, _sep) when is_nil(b) do
-    a
+  def join(a, b) do
+    IO.puts "***First join"
+    a <> b
   end
 
-  def join(a, b, sep) do
+  def join(a, b, sep \\ " ") do
+    IO.puts "***Second join"
     a <> sep <> b
   end
 end
-
-IO.puts Concat.join("Hello", "world")      #=> Hello world
-IO.puts Concat.join("Hello", "world", "_") #=> Hello_world
-IO.puts Concat.join("Hello")               #=> Hello
