1ca45334e93026c7ab76c4fa96a4659a1919aa06
Raul Pereira <raul.pereira@gmail.com>
2018-08-02 15:44:35 -0300

    Step 2: samples of recursion

--- apps/raulpe7eira/getting_started/math.exs
+++ apps/raulpe7eira/getting_started/math.exs
@@ -1,4 +1,11 @@
 defmodule Math do
-  def zero?(0), do: true
-  def zero?(x) when is_integer(x), do: false
+  def sum_list([head | tail], accumulator) do
+    sum_list(tail, head + accumulator)
+  end
+
+  def sum_list([], accumulator) do
+    accumulator
+  end
 end
+
+IO.puts Math.sum_list([1, 2, 3], 0) #=> 6
