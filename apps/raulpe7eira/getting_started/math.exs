90e284715cad5a0bbb36a5eac50fa1ebeba57943
Raul Pereira <raul.pereira@gmail.com>
2018-08-02 15:38:49 -0300

    Step 2: samples and modules and functions - updated

--- apps/raulpe7eira/getting_started/math.exs
+++ apps/raulpe7eira/getting_started/math.exs
@@ -1,14 +1,4 @@
 defmodule Math do
-  def zero?(0) do
-    true
-  end
-
-  def zero?(x) when is_integer(x) do
-    false
-  end
+  def zero?(0), do: true
+  def zero?(x) when is_integer(x), do: false
 end
-
-IO.puts Math.zero?(0)         #=> true
-IO.puts Math.zero?(1)         #=> false
-IO.puts Math.zero?([1, 2, 3]) #=> ** (FunctionClauseError)
-IO.puts Math.zero?(0.0)       #=> ** (FunctionClauseError)
