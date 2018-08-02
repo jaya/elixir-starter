f7d1247597df6f530d3182d2d9cb3b5d139562b4
Raul Pereira <raul.pereira@gmail.com>
2018-08-02 15:38:04 -0300

    Step 2: samples of modules and functions - updated

--- apps/raulpe7eira/getting_started/math.exs
+++ apps/raulpe7eira/getting_started/math.exs
@@ -1,12 +1,14 @@
 defmodule Math do
-  def sum(a, b) do
-    do_sum(a, b)
+  def zero?(0) do
+    true
   end
 
-  defp do_sum(a, b) do
-    a + b
+  def zero?(x) when is_integer(x) do
+    false
   end
 end
 
-IO.puts Math.sum(1, 2)    #=> 3
-IO.puts Math.do_sum(1, 2) #=> ** (UndefinedFunctionError)
+IO.puts Math.zero?(0)         #=> true
+IO.puts Math.zero?(1)         #=> false
+IO.puts Math.zero?([1, 2, 3]) #=> ** (FunctionClauseError)
+IO.puts Math.zero?(0.0)       #=> ** (FunctionClauseError)
