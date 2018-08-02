7f92e3fe2bf81b215aadd9b31fe6846ecde0ccfe
Raul Pereira <raul.pereira@gmail.com>
2018-08-02 15:37:05 -0300

    Step 2: samples of modules and functions - updated

--- apps/raulpe7eira/getting_started/math.exs
+++ apps/raulpe7eira/getting_started/math.exs
@@ -1,7 +1,12 @@
 defmodule Math do
   def sum(a, b) do
+    do_sum(a, b)
+  end
+
+  defp do_sum(a, b) do
     a + b
   end
 end
 
-IO.puts Math.sum(1, 2)
+IO.puts Math.sum(1, 2)    #=> 3
+IO.puts Math.do_sum(1, 2) #=> ** (UndefinedFunctionError)
