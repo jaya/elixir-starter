6546c828712b0cf5bd344140152b8d7f5d8d39bd
Raul Pereira <raul.pereira@gmail.com>
2018-08-02 19:26:15 -0300

    Step 2: samples of module attributes

--- apps/raulpe7eira/getting_started/math.ex
+++ apps/raulpe7eira/getting_started/math.ex
@@ -1,5 +1,16 @@
 defmodule Math do
-  def sum(a, b) do
-    a + b
-  end
+  @moduledoc """
+  Provides math-related functions.
+
+  ## Examples
+
+      iex> Math.sum(1, 2)
+      3
+
+  """
+
+  @doc """
+  Calculates the sum of two numbers.
+  """
+  def sum(a, b), do: a + b
 end
