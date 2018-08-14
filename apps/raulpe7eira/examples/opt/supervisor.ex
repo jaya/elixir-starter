5320d893bac7dd1e22f8611e455644825d709c68
Raul Pereira <raul.pereira@gmail.com>
2018-08-14 19:01:37 -0300

    Step 4: first item - ongoing (supervisor).

---
+++ apps/raulpe7eira/examples/opt/supervisor.ex
@@ -0,0 +1,15 @@
+defmodule KV.Supervisor do
+  use Supervisor
+
+  def start_link(opts) do
+    Supervisor.start_link(__MODULE__, :ok, opts)
+  end
+
+  def init(:ok) do
+    children = [
+      KV.Registry
+    ]
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+end
