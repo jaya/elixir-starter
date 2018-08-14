cc728d254ef28d308b17d856326a880cc59b356b
Raul Pereira <raul.pereira@gmail.com>
2018-08-14 19:25:59 -0300

    Reorganized examples folder.

--- apps/raulpe7eira/examples/mix_and_otp/kv/lib/kv/bucket.ex
+++ apps/raulpe7eira/examples/mix_and_otp/kv/lib/kv/bucket.ex
@@ -21,4 +21,13 @@ defmodule KV.Bucket do
   def put(bucket, key, value) do
     Agent.update(bucket, &Map.put(&1, key, value))
   end
+
+  @doc """
+  Deletes `key` from `bucket`.
+
+  Returns the current value of `key`, if `key` exists.
+  """
+  def delete(bucket, key) do
+    Agent.get_and_update(bucket, &Map.pop(&1, key))
+  end
 end
