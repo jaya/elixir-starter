3ba1065769906d7cef8ce538275ef5e12c21d6a1
Raul Pereira <raul.pereira@gmail.com>
2018-08-14 21:26:45 -0300

    Step 4: first item - done!

--- apps/raulpe7eira/examples/mix_and_otp/kv/lib/kv.ex
+++ apps/raulpe7eira/examples/mix_and_otp/kv/lib/kv.ex
@@ -1,18 +1,7 @@
 defmodule KV do
-  @moduledoc """
-  Documentation for KV.
-  """
+  use Application
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> KV.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def start(_type, _args) do
+    KV.Supervisor.start_link(name: KV.Supervisor)
   end
 end
