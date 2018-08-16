bb8bd3343d11cc697aad87bff72e21cd41aa1733
Raul Pereira <raul.pereira@gmail.com>
2018-08-16 02:23:48 -0300

    Step 5: pure otp exercises - ongoing (test:supervisor - done).

--- apps/raulpe7eira/exercises/pure_otp/test/todo/supervisor_test.exs
+++ apps/raulpe7eira/exercises/pure_otp/test/todo/supervisor_test.exs
@@ -1,8 +1,38 @@
 defmodule TODO.SupervisorTest do
-  use ExUnit.Case, async: false
+  use ExUnit.Case, async: true
   doctest TODO.Supervisor
   @moduletag :supervisor
 
-  # nothing implemented yet.
+  setup do
+    module = { TODO.Supervisor, name: TODO.Supervisor }
+    supervisor = start_supervised! module
+
+    %{ supervisor: supervisor, module: module }
+  end
+
+  describe "TODO.Supervisor.start_link/1" do
+    test "expected: a logger message, 'is started'", context do
+      assert is_pid context.supervisor
+    end
+
+    test "expected: a logger message, 'is already started'", context do
+      response = start_supervised context.module
+
+      assert { :error, { :already_started, _pid } } = response
+    end
+  end
+
+  describe "TODO.Supervisor[strategy: one_for_one]" do
+    test "expected: the restarted server", context do
+      [{ _id, child, _type, _modules } | _tail] = Supervisor.which_children context.supervisor
+
+      assert child == Process.whereis TODO.Server
+
+      Process.exit child, :kill
+      Process.sleep 1
+
+      assert child != Process.whereis TODO.Server
+    end
+  end
 
 end
