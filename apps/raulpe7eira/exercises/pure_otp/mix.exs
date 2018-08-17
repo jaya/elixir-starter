dad6774c31ccec3852206a41709d4dbdb601fd10
Raul Pereira <raul.pereira@gmail.com>
2018-08-17 12:44:09 -0300

    Defined aliases for test w/o start.

--- apps/raulpe7eira/exercises/pure_otp/mix.exs
+++ apps/raulpe7eira/exercises/pure_otp/mix.exs
@@ -7,7 +7,8 @@ defmodule PureOtp.MixProject do
       version: "0.1.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      aliases: aliases()
     ]
   end
 
@@ -24,6 +25,12 @@ defmodule PureOtp.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+    ]
+  end
+
+  defp aliases do
+    [
+      test: "test --no-start"
     ]
   end
 end
